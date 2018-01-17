8030ce83f7e3a61e8a2eb3e6677247c821688f69
Nydia Segura <nydia.segura@gmail.com>
2018-01-16 21:13:35 -0600

    first commit, index page first draft

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function(){
+    return gulp.src('./sass/**/style.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('watch', function(){
+    gulp.watch('./sass/**/*.scss', ['sass']);
+})
+
+gulp.task('default', ['sass','watch']);
+
