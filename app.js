37ec722365589bdb8bc0194788e2c6089a1df3b4
Daniel Dyer <danielbdyer@gmail.com>
2018-01-22 08:09:19 -0600

    Current version for project demo day

--- app.js
+++ app.js
@@ -3,118 +3,108 @@ const models = require("./models");
 const mustacheExpress = require("mustache-express");
 const bodyParser = require("body-parser");
 const path = require("path");
-const promise = require('bluebird');
-const methodOverride = require('method-override')
-const session = require('express-session')
+const promise = require("bluebird");
+const methodOverride = require("method-override");
+const session = require("express-session");
 const app = express();
 
 var currentshoppinglists;
 
 app.engine("mustache", mustacheExpress());
 app.set("views", "./views");
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 app.set("view engine", "mustache");
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 //register the session with its user id
-app.use(session({
-  secret:'cute dogs',
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: "cute dogs",
+    resave: false,
+    saveUninitialized: true
+  })
+);
 
+app.use(function(req, res, next) {
+  if (req.url.substr(-1) == "/" && req.url.length > 1)
+    res.redirect(301, req.url.slice(0, -1));
+  else next();
+});
 
 //sign up user and add to user table
-app.post('/signup',function(req,res){
+app.post("/signup", function(req, res) {
   var email = req.body.email;
   var password = req.body.password;
-  var username = email.split('@',1)[0];
+  var username = email.split("@", 1)[0];
 
-  const newUser = models.user.create({
-    email:email,
-    password:password,
-    username:username
-  }).then(function(newUser){
-    console.log(newUser);
-  }).then(res.redirect('/home'));
-})
-
+  const newUser = models.user
+    .create({
+      email: email,
+      password: password,
+      username: username
+    })
+    .then(function(newUser) {
+      console.log(newUser);
+    })
+    .then(res.redirect("/settings"))
+    .catch(function(err) {
+      console.log(err, req.body.email);
+    });
+});
 
 //log in the user
-app.post('/login', (req,res) => {
-  var user = models.user.findOne({
-    where:{
-      email : req.body.email,
-      password : req.body.password
-    }
-  }).then(user => {
-    if(user.password == req.body.password) {
-      req.session.email = req.body.email;
-      req.session.authenticated = true;
-      req.session.userId = user.dataValues.id;
-      res.redirect('/home');
-    } else {
-      res.send('Login attempt failed')
-    }
-  })
-})
+app.post("/login", (req, res) => {
+  var user = models.user
+    .findOne({
+      where: {
+        email: req.body.email,
+        password: req.body.password
+      }
+    })
+    .then(user => {
+      if (user.password == req.body.password) {
+        req.session.email = req.body.email;
+        req.session.authenticated = true;
+        req.session.userId = user.dataValues.id;
+        res.redirect("/posts/all");
+      } else {
+        res.send("Login attempt failed");
+      }
+    });
+});
 
 //to check if user is logged in
-app.get('/logged',function(req,res){
-  if(req.session.email){
+app.get("/logged", function(req, res) {
+  if (req.session.email) {
     res.write("<h1>User Logged In</h1><a href='/logout'>Logout</a>");
     res.end();
-  } else{
+  } else {
     res.write("<h1>User not logged in</h1><a href='/home'>Main Page</a>");
     res.end();
   }
-})
+});
 
 // logout the user
-app.get('/logout',function(req,res){
-  req.session.destroy(function(err){
-    if(err){
+app.get("/logout", function(req, res) {
+  req.session.destroy(function(err) {
+    if (err) {
       res.negotiate(err);
     }
     console.log("user is logged out");
-    res.redirect('/home');
-
+    res.redirect("/home");
   });
 });
 
-app.get("/habits/all/posts/:id", function(req, res) {
+app.get("posts/:id", function(req, res) {
   let reqPost = req.params.id;
   // get all the posts from the database using sequelize
   models.post
     .findAll({
-      where: {"id" : reqPost},
+      where: { id: reqPost },
       include: [
         {
           model: models.tag,
-          as: "tags",
-          }
-      ]
-    })
-    .then(function(posts) {
-      console.log(posts);
-      res.render("posts", { posts: posts });
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
-});
-
-app.get("/posts/all", function(req, res) {
-  // get all the posts from the database using sequelize
-  models.post
-    .findAll({
-      include: [
-        {
-          model: models.tag,
-          as: "tags",
-        },
-        { model: models.user,
-          as: "user"
+          as: "tags"
         }
       ]
     })
@@ -127,93 +117,195 @@ app.get("/posts/all", function(req, res) {
     });
 });
 
-
-
-app.post("/shoppinglists", function(req, res) {
-  let storename = req.body.storename;
-  let street = req.body.street;
-  let city = req.body.city;
-  let state = req.body.state;
-  let zip = req.body.zip;
-
-  models.shoppinglist
-    .create({
-      storename: storename,
-      street: street,
-      city: city,
-      state: state,
-      zip: zip
+app.get("/posts/all", function(req, res) {
+  var mustachevars = {}
+  // get all the posts from the database using sequelize
+  models.post
+    .findAll({
+      include: [
+        {
+          model: models.tag,
+          as: "tags"
+        },
+        {
+          model: models.user,
+          as: "user"
+        }
+      ]
     })
-    .then(function() {
-      res.redirect("back");
+    .then(function(allposts) {
+      mustachevars.result = allposts
+      models.tag.findAll()
+    .then(function(alltags){
+      mustachevars.result2 = alltags
+      console.log(alltags)
+      res.render("viewAllPosts", { posts: mustachevars.result, alltags: mustachevars.result2 });
     })
-    .catch(function(error) {
-      console.log(error);
-    });
-});
-
-app.post("/groceryitems/:id", function(req, res) {
-  let itemname = req.body.itemname;
-  let quantity = req.body.quantity;
-  let price = req.body.price;
-  let shoppinglistid = req.params.id;
-
-  models.groceryitem
-    .create({
-      itemname: itemname,
-      quantity: quantity,
-      price: price,
-      shoppinglistid: shoppinglistid
-    })
-    .then(function() {
-      res.redirect("back");
     })
     .catch(function(error) {
       console.log(error);
     });
 });
 
-app.get('/', (req,res) => {res.redirect('/home')});
+app.get("/topics/all", function(req, res) {
+  var mustachevars = {}
+  // get all the posts from the database using sequelize
+  const allposts = models.post
+    .findAll({
+      include: [
+        {
+          model: models.tag,
+          as: "tags"
+        },
+        {
+          model: models.user,
+          as: "user"
+        }
+      ]
+    })
+    .then(function(allposts) {
+      mustachevars.result = allposts
+      models.tag.findAll()
+    .then(function(alltags){
+      mustachevars.result2 = alltags
+      console.log(alltags)
+      res.render("viewAllPosts", { posts: mustachevars.result, alltags: mustachevars.result2 });
+    })
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
 
-//home page for local host is the index.mustache page
-app.get('/home', function(req, res){
-  if(req.session.email){
-    res.render('loggedInIndex',{email : req.session.email, userId:req.session.userId});
-  } else{
-    res.render('index');
+app.get("/newpost", function(req, res) {
+  if (req.session.email) {
+    const tag = models.tag.findAll().then(function(alltags) {
+      res.render("newPost", {
+        email: req.session.email,
+        username: req.session.username,
+        userId: req.session.userId,
+        alltags: alltags
+      });
+    });
+  } else {
+    res.render("index");
   }
 });
 
+app.get("/topics/:uris/", function(req, res) {
+    var mustachevars = {}
+  // get all the posts from the database using sequelize
+  models.post
+    .findAll({
+      include: [
+        {
+          model: models.tag,
+          where: { uri: req.params.uris.split("+") },
+          as: "tags"
+        },
+        {
+          model: models.user,
+          as: "user"
+        }
+      ]
+    })
+    .then(function(posts) {
+      mustachevars.result = posts
+      models.tag.findAll()
+    .then(function(alltags){
+      mustachevars.result2 = alltags
+      console.log(alltags)
+      res.render("viewAllPosts", { posts: mustachevars.result, alltags: mustachevars.result2 });
+    })
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+app.get("/posts/:id", function(req, res) {
+  // get all the posts from the database using sequelize
+  models.post
+    .findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: models.tag,
+          where: { uri: req.params.uri },
+          as: "tags"
+        },
+        {
+          model: models.user,
+          as: "user"
+        }
+      ]
+    })
+    .then(function(posts) {
+      console.log(posts);
+      res.render("viewSinglePost", { posts: posts });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+});
+
+app.post("/topics/:id/newpost", function(req, res) {
+  if (req.session.email) {
+    let postTitle = req.body.title;
+    let postSubheader = req.body.subheader;
+    let id = req.params.id
+  }
+})
+
+app.get("/", (req, res) => {
+  res.redirect("/home");
+});
+
+//home page for local host is the index.mustache page
+app.get("/home", function(req, res) {
+  if (req.session.email) {
+    res.render("loggedInIndex", {
+      email: req.session.email,
+      userId: req.session.userId
+    });
+  } else {
+    res.render("index");
+  }
+});
 
 //delete the user Account
-app.get('/deleteAccount/:id', function(req, res) {
+app.get("/deleteAccount/:id", function(req, res) {
   let userIdValue = req.params.id;
-  req.session.destroy(function(err){
-    if(err){
+  req.session.destroy(function(err) {
+    if (err) {
       res.negotiate(err);
     }
     console.log("user is logged out");
   });
-  var user = models.user.destroy({
-    where:{
-      id : userIdValue
-    }
-  }).then(function(user){
-    res.redirect('/home');
-  });
+  var user = models.user
+    .destroy({
+      where: {
+        id: userIdValue
+      }
+    })
+    .then(function(user) {
+      res.redirect("/home");
+    });
 });
 
-app.get('/settings', function(req, res) {
-  if(req.session.email){
-    const tag = models.tag.findAll()
-    .then(function(tags){
-    res.render('settings',{email : req.session.email, userId:req.session.userId,'tags':tags})
-  });
-  } else{
-    res.render('settingsNoLogin');
+app.get("/settings", function(req, res) {
+  if (req.session.email) {
+    const tag = models.tag.findAll().then(function(tags) {
+      res.render("settings", {
+        email: req.session.email,
+        userId: req.session.userId,
+        tags: tags
+      });
+    });
+  } else {
+    res.render("settingsNoLogin");
   }
 });
-
 
 app.get("/api/posts", function(req, res) {
   db.any("SELECT * FROM posts").then(function(data) {
