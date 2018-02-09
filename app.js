c6ec5b5ea14e0332fb4c593bfa4c4bc0c798ea24
Daniel Dyer <danielbdyer@gmail.com>
2018-02-08 18:08:44 -0600

    Fixing app.listen command to support Heroku

--- app.js
+++ app.js
@@ -313,6 +313,6 @@ app.get("/api/posts", function(req, res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("node server has started");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
